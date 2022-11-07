ee25636a1ebeaefd979e2d1908fbc0f065c12bd5
wesleysousuke <wesleysousukee@gmail.com>
2022-11-07 08:25:12 -0300

    Adicionar arquivos de projeto.

---
+++ FilmesApi/Data/AppDbContext.cs
@@ -0,0 +1,31 @@
+﻿using FilmesApi.Models;
+using FilmesAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmesApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            ///<summary>
+            /// - A entidade do tipo Endereco tem um Cinema
+            /// - O Cinema também possui um Endereco
+            /// - A referência para os elementos das tabelas estão em cinema.EnderecoId
+            ///</summary>
+            builder.Entity<Endereco>()
+                .HasOne(endereco => endereco.Cinema)
+                .WithOne(cinema => cinema.Endereco)
+                .HasForeignKey<Cinema>(cinema => cinema.EnderecoId);
+        }
+
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+    }
+}
